16de269c08c3d099cadf1dcfb76f803fc0d9f8cf
Od saleva <misha.vidiakin@gmail.com>
2023-11-08 21:32:17 +0300

    save

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <form @submit.prevent="registration">
+    <p>
+      <label for="text">login: </label><input v-model="login" type="text" />
+    </p>
+    <p>
+      <label for="psw">password: </label
+      ><input v-model="password" type="password" />
+    </p>
+    <p><input type="submit" /></p>
+  </form>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      login: "",
+      password: "",
+      response: null,
+    };
+  },
+  computed: {
+    host() {
+      return "http://localhost:7123";
+    },
+    submitForm() {
+      return {
+        login: this.login,
+        password: this.password,
+      };
+    },
+  },
+  methods: {
+    async registration() {
+      axios
+        .post(`${this.host}/auth/registration`, this.submitForm)
+        .then((response) => {
+          console.log(this.submitForm);
+          this.response = response.data;
+          console.log(this.response);
+        })
+        .catch((e) => {
+          console.log("Error registration() - ", e);
+        });
+    },
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
